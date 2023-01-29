cf860bbc4f3d1ab6ec5aa859fd88bf95eb475a39
lzgrzebski <myczek18@gmail.com>
2023-01-29 13:50:52 +0100

    feat: add basic state

---
+++ src/state.ts
@@ -0,0 +1,26 @@
+import { WebsocketProvider } from 'y-websocket';
+import * as Y from 'yjs';
+import { v4 as uuid } from 'uuid';
+
+import { COLORS, PROVIDER_URL } from './constants';
+import type { Point } from './types';
+
+export const getInitialState = (id: string) => {
+    const doc = new Y.Doc();
+    const elements = doc.getArray<Y.Map<Y.Array<Point> | string>>('elements');
+
+    const provider = new WebsocketProvider(PROVIDER_URL, id, doc, {
+        connect: false,
+    });
+
+    return { awareness: provider.awareness, doc, elements, provider };
+};
+
+export const generateUser = (name: string) => {
+    const id = uuid();
+    const color = COLORS[Math.floor(Math.random() * COLORS.length)];
+
+    return { color, id, name };
+};
+
+export type Store = ReturnType<typeof getInitialState>;
