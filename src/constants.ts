6faa43780c885ceb2a3a9b3a3442baa1416214c2
lzgrzebski <myczek18@gmail.com>
2023-01-29 13:50:22 +0100

    feat: add constants file

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const COLORS = ['#CCA43B', '#36558F', '#40376E', '#40376E', '#D36582'];
+
+export const HOST = import.meta.env.HOST ?? 'localhost';
+export const PORT = import.meta.env.PORT ?? '3214';
+export const PROVIDER_URL = `${
+    HOST === 'localhost' ? 'ws' : 'wss'
+}://${HOST}:${PORT}`;
+
+export const STROKE_OPTIONS = {
+    size: 20,
+    smoothing: 0.5,
+    streamline: 0.5,
+    thinning: 0.5,
+} as const;
