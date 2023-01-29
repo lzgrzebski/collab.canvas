d30820ec58377330575903a90e4918772cf8ecec
lzgrzebski <myczek18@gmail.com>
2023-01-29 13:30:41 +0100

    feat: tweak User component

--- src/views/User/User.view.tsx
+++ src/views/User/User.view.tsx
@@ -4,13 +4,14 @@ import cx from 'classnames';
 import styles from './User.module.css';
 
 export const User: React.FC<
-    React.ComponentProps<'div'> & { color: string }
+    React.ComponentProps<'div'> & { color: string; children: string }
 > = ({ className, color, children, ...props }) => (
     <div
         className={cx(styles.user, className)}
         style={{ backgroundColor: color }}
+        title={children}
         {...props}
     >
-        <span className={styles.initials}>{children}</span>
+        <span className={styles.initials}>{(children ?? '?').charAt(0)}</span>
     </div>
 );
